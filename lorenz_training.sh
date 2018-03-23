8cd15584f5bfcb53f9620d0fcafbebc8a8b38209
djgagne@ou.edu <djgagne@ou.edu>
2018-03-23 15:29:09 -0600

    Added all the new yaml files

--- lorenz_training.sh
+++ lorenz_training.sh
@@ -2,7 +2,7 @@
 #PBS -N lorenz_train
 #PBS -A P54048000
 #PBS -q regular
-#PBS -l walltime=01:00:00
+#PBS -l walltime=02:00:00
 #PBS -l select=1:ncpus=36:ompthreads=36
 #PBS -m abe
 #PBS -M dgagne@ucar.edu
@@ -12,3 +12,5 @@ source /glade/u/home/dgagne/.bash_profile
 source activate deep
 cd /glade/u/home/dgagne/lorenz_gan
 python -u train_lorenz_gan.py config/lorenz_gan_u_cheyenne.yaml -g
+python -u train_lorenz_gan.py config/lorenz_gan_u_chey_d_2.yaml -r -g
+python -u train_lorenz_gan.py config/lorenz_gan_u_chey_d_0.yaml -r -g
