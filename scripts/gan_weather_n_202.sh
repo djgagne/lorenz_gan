231ed57e166876210cd5033f64105f536ad1d151
djgagne@ou.edu <djgagne@ou.edu>
2019-02-16 16:14:15 -0700

    Updated config files and jupyter notebooks

--- scripts/gan_weather_n_202.sh
+++ scripts/gan_weather_n_202.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #PBS -N gan_w_202
-#PBS -A P54048000
+#PBS -A NAML0001
 #PBS -q regular
-#PBS -l walltime=10:00:00
-#PBS -l select=1:ncpus=36:ompthreads=36:mem=109GB
+#PBS -l walltime=3:00:00
+#PBS -l select=1:ncpus=36:ompthreads=36
 #PBS -m abe
 #PBS -M dgagne@ucar.edu
 #PBS -j oe
 module unload ncarenv
 source /glade/u/home/dgagne/.bash_profile
-source activate deep
+export PATH="/glade/u/home/dgagne/miniconda3/envs/ml/bin:$PATH"
 cd /glade/u/home/dgagne/lorenz_gan
-python -u run_lorenz_forecast.py config/forecast_20_gan_n_202_c_dense.yaml -p 36 >& weather_gan_202.log
+python -u run_lorenz_forecast.py config/exp_20_stoch/forecast_20_gan_n_202_c_dense.yaml -p 36 >& weather_gan_202.log
