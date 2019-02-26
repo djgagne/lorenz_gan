Fixed error in residual calculation.

lorenz_gan/analysis.py
@@ -7,6 +7,7 @@ from glob import glob
 from os.path import join
 import keras.backend as K
 
+
 def load_test_data(filename,
                    input_columns=("X_t", "Ux_t"),
                    output_columns=("Ux_t+1",),
@@ -46,9 +47,9 @@ def offline_gan_predictions(gan_index, data,
         rand_size = 17
     random_values = rs.normal(size=(data.shape[0], rand_size))
     all_zeros = np.zeros((data.shape[0], rand_size), dtype=np.float32)
-    corr_noise = np.zeros((data.shape[0], rand_size), dtype=np.float32)
+    #corr_noise = np.zeros((data.shape[0], rand_size), dtype=np.float32)
     gen_preds = dict()
-    for pred_type in ["det", "rand", "corr"]:
+    for pred_type in ["det", "rand"]:
         gen_preds[pred_type] = pd.DataFrame(0, index=data.index, columns=gen_filenames,
                              dtype=np.float32)
     gen_noise = pd.DataFrame(0.0, dtype=np.float32, index=gen_filenames, columns=["corr", "noise_sd"])
@@ -80,19 +81,19 @@ def offline_gan_predictions(gan_index, data,
             ar1.fit(data.loc[x_indices, "Ux_t+1"].values - gen_preds["det"].loc[x_indices, gen_filenames[g]].values)
             print(gen_filenames[g], ar1.corr, ar1.noise_sd)
             gen_noise.loc[gen_filenames[g]] = [ar1.corr, ar1.noise_sd]
-            rs_corr = np.random.RandomState(seed)
-            corr_noise[0] = rs_corr.normal(size=(1, rand_size))
-            print(gen_filenames[g], "random noise")
-            for i in range(1, corr_noise.shape[0]):
-                corr_noise[i] = ar1.update(corr_noise[i - 1], rs_corr)
-            print(gen_filenames[g], "Corr preds")
-            gen_preds["corr"].loc[:, gen_filenames[g]] = gen_model.predict_batch(data[input_cols],
-                                                                                    corr_noise, batch_size=batch_size,
-                                                                                    stochastic=1)
+            #rs_corr = np.random.RandomState(seed)
+            #corr_noise[0] = rs_corr.normal(size=(1, rand_size))
+            #print(gen_filenames[g], "random noise")
+            #for i in range(1, corr_noise.shape[0]):
+            #    corr_noise[i] = ar1.update(corr_noise[i - 1], rs_corr)
+            #print(gen_filenames[g], "Corr preds")
+            #gen_preds["corr"].loc[:, gen_filenames[g]] = gen_model.predict_batch(data[input_cols],
+            #                                                                        corr_noise, batch_size=batch_size,
+            #                                                                        stochastic=1)
             del ar1
             del gen_model.model
             del gen_model
-            corr_noise[:] = 0
+            #corr_noise[:] = 0
         sess.close()
         del sess
     return gen_preds, gen_noise

lorenz_gan/lorenz.py
@@ -225,14 +225,13 @@ def lorenz_96_forecast_step_ivp(time, x_input, f=20, time_step=0.005, parameteri
     return dXdt
 
 
-def process_lorenz_data(X_out, Y_out, times, steps, J, F, dt, x_skip, t_skip, u_scale):
+def process_lorenz_data(X_out, times, steps, J, F, dt, x_skip, t_skip, u_scale):
     """
     Sample from Lorenz model output and reformat the data into a format more amenable to machine learning.
 
 
     Args:
         X_out (ndarray): Lorenz 96 model output
-        Y_out (ndarray):
         J (int): number of Y variables per X variable
         x_skip (int): number of X variables to skip when sampling the data
         t_skip (int): number of time steps to skip when sampling the data
@@ -241,8 +240,8 @@ def process_lorenz_data(X_out, Y_out, times, steps, J, F, dt, x_skip, t_skip, u_
         combined_data: pandas DataFrame
     """
     x_series_list = []
-    y_series_list = []
-    y_prev_list = []
+    #y_series_list = []
+    #y_prev_list = []
     ux_series_list = []
     ux_prev_series_list = []
     u_series_list = []
@@ -260,17 +259,18 @@ def process_lorenz_data(X_out, Y_out, times, steps, J, F, dt, x_skip, t_skip, u_
                               (X_out[t_s + 1, k] - X_out[t_s, k]) / dt)
         ux_prev_series_list.append((-X_out[t_p, k - 1] * (X_out[t_p, k - 2] - X_out[t_p, (k + 1) % K]) - X_out[t_p, k]
                                     + F) - (X_out[t_s, k] - X_out[t_p, k]) / dt)
-        y_series_list.append(Y_out[t_s, k * J: (k + 1) * J])
-        y_prev_list.append(Y_out[t_p, k * J: (k + 1) * J])
-        u_series_list.append(np.expand_dims(u_scale * Y_out[t_s, k * J: (k+1) * J].sum(axis=1), 1))
-        u_prev_series_list.append(np.expand_dims(u_scale * Y_out[t_p, k * J: (k+1) * J].sum(axis=1), 1))
+        #y_series_list.append(Y_out[t_s, k * J: (k + 1) * J])
+        #y_prev_list.append(Y_out[t_p, k * J: (k + 1) * J])
+        #u_series_list.append(np.expand_dims(u_scale * Y_out[t_s, k * J: (k+1) * J].sum(axis=1), 1))
+        #u_prev_series_list.append(np.expand_dims(u_scale * Y_out[t_p, k * J: (k+1) * J].sum(axis=1), 1))
         time_list.append(times[t_s])
         step_list.append(steps[t_s])
         x_list.append(np.ones(time_list[-1].size) * k)
     x_cols = ["X_t"]
-    y_cols = ["Y_t+1_{0:d}".format(y) for y in range(J)]
-    y_p_cols = ["Y_t_{0:d}".format(y) for y in range(J)]
-    u_cols = ["Uy_t", "Uy_t+1", "Ux_t", "Ux_t+1"]
+    #y_cols = ["Y_t+1_{0:d}".format(y) for y in range(J)]
+    #y_p_cols = ["Y_t_{0:d}".format(y) for y in range(J)]
+    #u_cols = ["Uy_t", "Uy_t+1", "Ux_t", "Ux_t+1"]
+    u_cols = ["Ux_t", "Ux_t+1"]
     combined_data = pd.DataFrame(np.vstack(x_series_list), columns=x_cols)
     combined_data.loc[:, "time"] = np.concatenate(time_list)
     combined_data.loc[:, "step"] = np.concatenate(step_list)
@@ -278,11 +278,11 @@ def process_lorenz_data(X_out, Y_out, times, steps, J, F, dt, x_skip, t_skip, u_
     combined_data.loc[:, "u_scale"] = u_scale
     combined_data.loc[:, "Ux_t+1"] = np.concatenate(ux_series_list)
     combined_data.loc[:, "Ux_t"] = np.concatenate(ux_prev_series_list)
-    combined_data.loc[:, "Uy_t+1"] = np.concatenate(u_series_list)
-    combined_data.loc[:, "Uy_t"] = np.concatenate(u_prev_series_list)
-    combined_data = pd.concat([combined_data, pd.DataFrame(np.vstack(y_prev_list), columns=y_p_cols),
-                               pd.DataFrame(np.vstack(y_series_list), columns=y_cols)], axis=1)
-    out_cols = ["x_index", "step", "time", "u_scale"] + x_cols + u_cols + y_p_cols + y_cols
+    #combined_data.loc[:, "Uy_t+1"] = np.concatenate(u_series_list)
+    #combined_data.loc[:, "Uy_t"] = np.concatenate(u_prev_series_list)
+    #combined_data = pd.concat([combined_data, pd.DataFrame(np.vstack(y_prev_list), columns=y_p_cols),
+    #                           pd.DataFrame(np.vstack(y_series_list), columns=y_cols)], axis=1)
+    out_cols = ["x_index", "step", "time", "u_scale"] + x_cols + u_cols # + y_p_cols + y_cols
     return combined_data.loc[:, out_cols]
 
 

train_lorenz_gan.py
@@ -1,7 +1,7 @@
 from lorenz_gan.lorenz import run_lorenz96_truth, process_lorenz_data, save_lorenz_output
 from lorenz_gan.gan import generator_conv, generator_dense, discriminator_conv, discriminator_dense
 from lorenz_gan.gan import predict_stochastic, generator_dense_stoch, discriminator_conv_concrete
-from lorenz_gan.gan import train_gan, initialize_gan, normalize_data, generator_conv_concrete
+from lorenz_gan.gan import train_gan, initialize_gan, normalize_data, generator_conv_concrete, unnormalize_data
 from lorenz_gan.submodels import AR1RandomUpdater, SubModelHist, SubModelPoly, SubModelPolyAdd, SubModelANNRes
 import xarray as xr
 import keras.backend as K
@@ -218,9 +218,10 @@ def train_lorenz_gan(config, combined_data, combined_time_series):
               rand_vec_length, config["gan"]["gan_path"],
               config["gan"]["gan_index"], config["gan"]["num_epochs"], config["gan"]["metrics"])
     gen_pred_func = predict_stochastic(gen_model)
-    gen_ts_preds = gen_pred_func([combined_time_series[x_cols],
-                                  np.zeros((combined_time_series.shape[0],
-                                            rand_vec_length))])[0]
+    gen_ts_preds = unnormalize_data(gen_pred_func([combined_time_series[x_cols],
+                                                   np.zeros((combined_time_series.shape[0],
+                                                             rand_vec_length)),
+                                                   0])[0], scaling_values=Y_scaling_values)
     gen_ts_residuals = combined_time_series[y_cols].values.ravel() - gen_ts_preds.ravel()
     train_random_updater(gen_ts_residuals,
                          config["random_updater"]["out_file"].replace(".pkl",