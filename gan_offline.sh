8c7dece0bda011cf18dcb6e02aec72c683ce86fd
djgagne@ou.edu <djgagne@ou.edu>
2019-03-28 14:16:27 -0600

    other settings

--- gan_offline.sh
+++ gan_offline.sh
@@ -2,7 +2,7 @@
 #PBS -N off_gan
 #PBS -A NAML0001
 #PBS -q regular
-#PBS -l walltime=12:00:00
+#PBS -l walltime=8:00:00
 #PBS -l select=1:ncpus=36:ompthreads=36
 #PBS -m abe
 #PBS -M dgagne@ucar.edu
@@ -13,4 +13,4 @@ source /glade/u/home/dgagne/.bashrc
 export PATH="/glade/u/home/dgagne/miniconda3/envs/ml/bin:$PATH"
 cd /glade/u/home/dgagne/lorenz_gan
 python setup.py install
-python -u gan_offline_analysis.py config/exp_20_stoch/gan_offline_config.yaml -n 3 &> offline_gan.log
+python -u gan_offline_analysis.py config/exp_20_stoch/gan_offline_config.yaml -n 7 &> offline_gan.log
--- scripts/lorenz_training.sh
+++ scripts/lorenz_training.sh
@@ -2,14 +2,14 @@
 #PBS -N lorenz_train
 #PBS -A NAML0001
 #PBS -q regular
-#PBS -l walltime=03:00:00
+#PBS -l walltime=02:00:00
 #PBS -l select=1:ncpus=36:ompthreads=36
 #PBS -m abe
 #PBS -M dgagne@ucar.edu
 #PBS -j oe
 module unload ncarenv
 source /glade/u/home/dgagne/.bash_profile
-export PATH="/glade/u/home/dgagne/miniconda3/envs/deep/bin/:$PATH"
+export PATH="/glade/u/home/dgagne/miniconda3/envs/ml/bin/:$PATH"
 cd /glade/u/home/dgagne/lorenz_gan
 #python -u train_lorenz_gan.py config/lorenz_train_f_20_n_000_c_dense.yaml -g &> lorenz_train.log
 #declare -a configs=("100" "101" "102" "103" "202" "203" "300" "301" "302" "303" "402" "403")
